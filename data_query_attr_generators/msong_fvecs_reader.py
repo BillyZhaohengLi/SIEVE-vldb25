9cfd5689329511a934b117ad4f2c34f92b61dbf2
BillyZhaohengLi <zl20@yongjoo-serv-01.cs.illinois.edu>
2025-07-29 18:04:01 -0500

    refactor code in data generators

--- data_query_attr_generators/msong_fvecs_reader.py
+++ data_query_attr_generators/msong_fvecs_reader.py
@@ -1,160 +1,30 @@
-import random
-import math
 import numpy as np
-from scipy.sparse import csc_matrix, lil_matrix, csr_matrix
+import math
 import pickle
-from heapq import heapify, heappush, heappop 
+import random
 
-def write_sparse_matrix(mat, fname):
-    """ write a CSR matrix in the spmat format """
-    with open(fname, "wb") as f:
-        sizes = np.array([mat.shape[0], mat.shape[1], mat.nnz], dtype='int64')
-        sizes.tofile(f)
-        indptr = mat.indptr.astype('int64')
-        indptr.tofile(f)
-        mat.indices.astype('int32').tofile(f)
-        mat.data.astype('float32').tofile(f)
-
-def read_sparse_matrix_fields(fname):
-    """ read the fields of a CSR matrix without instanciating it """
-    with open(fname, "rb") as f:
-        sizes = np.fromfile(f, dtype='int64', count=3)
-        nrow, ncol, nnz = sizes
-        indptr = np.fromfile(f, dtype='int64', count=nrow + 1)
-        assert nnz == indptr[-1]
-        indices = np.fromfile(f, dtype='int32', count=nnz)
-        assert np.all(indices >= 0) and np.all(indices < ncol)
-        data = np.fromfile(f, dtype='float32', count=nnz)
-        return data, indices, indptr, ncol
-
-def mmap_sparse_matrix_fields(fname):
-    """ mmap the fields of a CSR matrix without instanciating it """
-    with open(fname, "rb") as f:
-        sizes = np.fromfile(f, dtype='int64', count=3)
-        nrow, ncol, nnz = sizes
-    ofs = sizes.nbytes
-    indptr = np.memmap(fname, dtype='int64', mode='r', offset=ofs, shape=nrow + 1)
-    ofs += indptr.nbytes
-    indices = np.memmap(fname, dtype='int32', mode='r', offset=ofs, shape=nnz)
-    ofs += indices.nbytes
-    data = np.memmap(fname, dtype='float32', mode='r', offset=ofs, shape=nnz)
-    return data, indices, indptr, ncol
-
-def read_sparse_matrix(fname, do_mmap=False):
-    """ read a CSR matrix in spmat format, optionally mmapping it instead """
-    if not do_mmap:
-        data, indices, indptr, ncol = read_sparse_matrix_fields(fname)
-    else:
-        data, indices, indptr, ncol = mmap_sparse_matrix_fields(fname)
-
-    return csr_matrix((data, indices, indptr), shape=(len(indptr) - 1, ncol))
-
-def fvecs_read(filename, dtype, c_contiguous=True):
-    fv = np.fromfile(filename, dtype=dtype)
-
-    if fv.size == 0:
-       return np.zeros((0, 0))
-    dim = fv.view(np.int32)[0]
-    # print("dim:", fv.view(np.int32))
-    # print(len(fv.view(np.int32)))
-    assert dim > 0
-    fv = fv.reshape(-1, 1 + dim)
-    if not all(fv.view(np.int32)[:, 0] == dim):
-       raise IOError("Non-uniform vector sizes in " + filename)
-    fv = fv[:, 1:]
-    if c_contiguous:
-       fv = fv.copy()
-    print(fv.shape)
-    return fv
-
-
-def read_fbin(filename, start_idx=0, chunk_size=None):
-    """ Read *.fbin file that contains float32 vectors
-    Args:
-        :param filename (str): path to *.fbin file
-        :param start_idx (int): start reading vectors from this index
-        :param chunk_size (int): number of vectors to read.
-                                 If None, read all vectors
-    Returns:
-        Array of float32 vectors (numpy.ndarray)
-    """
-    with open(filename, "rb") as f:
-        nvecs, dim = np.fromfile(f, count=2, dtype=np.int32)
-        nvecs = (nvecs - start_idx) if chunk_size is None else chunk_size
-        arr = np.fromfile(f, count=nvecs * dim, dtype=np.float32,
-                          offset=start_idx * 4 * dim)
-    return arr.reshape(nvecs, dim)
-
-def ivecs_read(fname):
-    a = np.fromfile(fname, dtype='int32')
-    d = a[0]
-    return a.reshape(-1, d + 1)[:, 1:].copy()
-
-def setup_dataset(dataset, queries):
-    # Generate data filters
-    num_points = dataset.shape[0]
-    dim = dataset.shape[1]
-
-    # One-hot attributes
-    # Following yfcc-10M, 200000 keywords as tags following harmonic distribution
-    # i.e., the nth tag has probability 1/n of being assigned to a vector
-    # We start at n=3.
-
-    n_filters = 200000
-    random.seed(10)
-    
-    data_filters_lil_matrix = lil_matrix((dataset.shape[0], n_filters), dtype=np.int32)
-    for j in range(n_filters):
-        if j % 1000 == 0:
-            print(j)
-        target = 1 / (j + 3)
-        for i in range(num_points):
-            if random.random() < target:
-                data_filters_lil_matrix[i, j] = 1
-    print("nonzero:", data_filters_lil_matrix.count_nonzero())
-    data_filters_csc_matrix = csc_matrix(data_filters_lil_matrix)
-    pickle.dump(data_filters_csc_matrix, open("/home/zl20/bigann/biganntest/data/msong/msong_query_attrs.pkl", "wb"))
-    
-
-    # n_filters = 200000
-    # random.seed(10)
-    # data_filters_lil_matrix = lil_matrix((dataset.shape[0], n_filters), dtype=np.int32)
-    # for j in range(n_filters):
-    #     if j % 1000 == 0:
-    #         print(j)
-    #     target = 1 / (j + 3)
-    #     for i in range(num_points):
-    #         if random.random() < target:
-    #             data_filters_lil_matrix[i, j] = 1
-    # print("nonzero:", data_filters_lil_matrix.count_nonzero())
-    # data_filters_csc_matrix = csc_matrix(data_filters_lil_matrix)
-    # pickle.dump(data_filters_csc_matrix, open("/home/zl20/composite-indexing-datasets/paper/paper/paper_query_attrs.pkl", "wb"))
-
-    # One-hot attributes
-    
-
+from scipy.sparse import lil_matrix, csc_matrix
+from utils import fvecs_read
 
 if __name__ == "__main__":
-    query_matrix = fvecs_read("~/bigann/biganntest/data/msong/msong_query.fvecs", np.int32)
-    dataset = fvecs_read("~/bigann/biganntest/data/msong/msong_base.fvecs", np.int32)
-
-    print(query_matrix.shape)
-    print(dataset.shape)
+    query_matrix = fvecs_read("biganntest/data/msong/msong_query.fvecs", np.int32)
+    dataset = fvecs_read("biganntest/data/msong/msong_base.fvecs", np.int32)
 
     # 19 attrs, each with (i * 0.05) selectivity, 1 <= i <= 19
-    data_filters_lil_matrix = lil_matrix((dataset.shape[0], 19), dtype=np.int32)
     random.seed(10)
-    for i in range(19):
-        target = (i + 1) * 0.05
-        for j in range(dataset.shape[0]):
-            if random.random() <= target:
-                data_filters_lil_matrix[j, i] = 1
-    pickle.dump(data_filters_lil_matrix, open("~/bigann/biganntest/data/msong/msong_data_attrs.pkl", "wb"))
+    n_filters = 19
+    data_filters = lil_matrix((dataset.shape[0], 19), dtype=np.int32)
+    for i in range(n_filters):
+        selectivity = (i + 1) * 0.05
+        mask = np.random.rand(dataset.shape[0]) <= selectivity
+        data_filters[:, i] = mask.astype(np.int32)
+    pickle.dump(data_filters, open("biganntest/data/msong/msong_data_attrs.pkl", "wb"))
 
-    # 8 queries for each attr
-    query_filters_lil_matrix = lil_matrix((query_matrix.shape[0], 19))
+    # 8 queries for each attr; other 200 - 19 * 8 = 48 queries have no filters.
+    query_filters = lil_matrix((query_matrix.shape[0], n_filters))
     for i in range(query_matrix.shape[0]):
-        if i % 25 < 19:
-            query_filters_lil_matrix[i, i % 25] = 1
-    pickle.dump(query_filters_lil_matrix, open("~/bigann/biganntest/data/msong/msong_query_attrs.pkl", "wb"))
+        if i % 25 < n_filters:
+            query_filters[i, i % 25] = 1
+
+    pickle.dump(csc_matrix(query_filters), open("biganntest/data/msong/msong_query_attrs.pkl", "wb"))
 
--- data_query_attr_generators/uqv_fvecs_reader.py
+++ data_query_attr_generators/uqv_fvecs_reader.py
@@ -1,164 +1,86 @@
-import random
-import math
 import numpy as np
-from scipy.sparse import csc_matrix, lil_matrix, csr_matrix
+import math
 import pickle
-from heapq import heapify, heappush, heappop 
+import random
 
-def write_sparse_matrix(mat, fname):
-    """ write a CSR matrix in the spmat format """
-    with open(fname, "wb") as f:
-        sizes = np.array([mat.shape[0], mat.shape[1], mat.nnz], dtype='int64')
-        sizes.tofile(f)
-        indptr = mat.indptr.astype('int64')
-        indptr.tofile(f)
-        mat.indices.astype('int32').tofile(f)
-        mat.data.astype('float32').tofile(f)
-
-def read_sparse_matrix_fields(fname):
-    """ read the fields of a CSR matrix without instanciating it """
-    with open(fname, "rb") as f:
-        sizes = np.fromfile(f, dtype='int64', count=3)
-        nrow, ncol, nnz = sizes
-        indptr = np.fromfile(f, dtype='int64', count=nrow + 1)
-        assert nnz == indptr[-1]
-        indices = np.fromfile(f, dtype='int32', count=nnz)
-        assert np.all(indices >= 0) and np.all(indices < ncol)
-        data = np.fromfile(f, dtype='float32', count=nnz)
-        return data, indices, indptr, ncol
-
-def mmap_sparse_matrix_fields(fname):
-    """ mmap the fields of a CSR matrix without instanciating it """
-    with open(fname, "rb") as f:
-        sizes = np.fromfile(f, dtype='int64', count=3)
-        nrow, ncol, nnz = sizes
-    ofs = sizes.nbytes
-    indptr = np.memmap(fname, dtype='int64', mode='r', offset=ofs, shape=nrow + 1)
-    ofs += indptr.nbytes
-    indices = np.memmap(fname, dtype='int32', mode='r', offset=ofs, shape=nnz)
-    ofs += indices.nbytes
-    data = np.memmap(fname, dtype='float32', mode='r', offset=ofs, shape=nnz)
-    return data, indices, indptr, ncol
-
-def read_sparse_matrix(fname, do_mmap=False):
-    """ read a CSR matrix in spmat format, optionally mmapping it instead """
-    if not do_mmap:
-        data, indices, indptr, ncol = read_sparse_matrix_fields(fname)
-    else:
-        data, indices, indptr, ncol = mmap_sparse_matrix_fields(fname)
-
-    return csr_matrix((data, indices, indptr), shape=(len(indptr) - 1, ncol))
-
-def fvecs_read(filename, dtype, c_contiguous=True):
-    fv = np.fromfile(filename, dtype=dtype)
-
-    if fv.size == 0:
-       return np.zeros((0, 0))
-    dim = fv.view(np.int32)[0]
-    # print("dim:", fv.view(np.int32))
-    # print(len(fv.view(np.int32)))
-    assert dim > 0
-    fv = fv.reshape(-1, 1 + dim)
-    if not all(fv.view(np.int32)[:, 0] == dim):
-       raise IOError("Non-uniform vector sizes in " + filename)
-    fv = fv[:, 1:]
-    if c_contiguous:
-       fv = fv.copy()
-    print(fv.shape)
-    return fv
-
-
-def read_fbin(filename, start_idx=0, chunk_size=None):
-    """ Read *.fbin file that contains float32 vectors
-    Args:
-        :param filename (str): path to *.fbin file
-        :param start_idx (int): start reading vectors from this index
-        :param chunk_size (int): number of vectors to read.
-                                 If None, read all vectors
-    Returns:
-        Array of float32 vectors (numpy.ndarray)
-    """
-    with open(filename, "rb") as f:
-        nvecs, dim = np.fromfile(f, count=2, dtype=np.int32)
-        nvecs = (nvecs - start_idx) if chunk_size is None else chunk_size
-        arr = np.fromfile(f, count=nvecs * dim, dtype=np.float32,
-                          offset=start_idx * 4 * dim)
-    return arr.reshape(nvecs, dim)
-
-def ivecs_read(fname):
-    a = np.fromfile(fname, dtype='int32')
-    d = a[0]
-    return a.reshape(-1, d + 1)[:, 1:].copy()
-
+from scipy.sparse import lil_matrix, csc_matrix
+from utils import fvecs_read
 
 if __name__ == "__main__":
-    query = fvecs_read("~/bigann/biganntest/data/uqv/uqv_query.fvecs", np.int32)
-    dataset = fvecs_read("~/bigann/biganntest/data/uqv/uqv_base.fvecs", np.int32)
+    query = fvecs_read("biganntest/data/uqv/uqv_query.fvecs", np.int32)
+    dataset = fvecs_read("biganntest/data/uqv/uqv_base.fvecs", np.int32)
 
-    n_filters = 200000
+    # 200K data attrs, each drawn from zipf distribution
     random.seed(10)
-    data_filters_lil_matrix = lil_matrix((dataset.shape[0], n_filters), dtype=np.int32)
+    n_filters = 20
+    data_filters = lil_matrix((dataset.shape[0], n_filters), dtype=np.int32)
     for j in range(n_filters):
-        if j % 1000 == 0:
-            print(j)
-        target = 1 / (j + 3)
-        for i in range(num_points):
-            if random.random() < target:
-                data_filters_lil_matrix[i, j] = 1
-    print("nonzero:", data_filters_lil_matrix.count_nonzero())
-    data_filters_csc_matrix = csc_matrix(data_filters_lil_matrix)
-    pickle.dump(data_filters_csc_matrix, open("~/bigann/biganntest/data/uqv/uqv_query_attrs.pkl", "wb"))
-    spmat = pickle.load(open("~/bigann/biganntest/data/uqv/uqv_query_attrs.pkl", "rb"))
-    filter_list = [i for i in range(200000)]
+        prob = 1 / (j + 3)
+        mask = np.random.rand(dataset.shape[0]) < prob
+        data_filters[:, j] = mask.astype(np.int32).reshape(-1, 1)
+    print("Nonzero entries:", data_filters_lil_matrix.count_nonzero())
+    data_filters_csc = csc_matrix(data_filters_lil_matrix)
+    pickle.dump(data_filters_csc, open("biganntest/data/uqv/uqv_query_attrs.pkl", "wb"))
 
-    sum_prob = sum([math.pow(1/(j+3), 0.75) for j in range(200000)])
-    probs = [math.pow(1/(j+3), 0.75) / sum_prob for j in range(200000)]
+    filter_list = list(range(200000))
 
-    count_list = [i for i in range(3, max_filters)]
-    count_sum_prob = sum([1/(j+3) for j in range(3, max_filters)])
-    count_probs = [1/(j+3) / count_sum_prob for j in range(3, max_filters)]
+    # Precompute zipf probabilities for filter selection 
+    probs = np.array([math.pow(1/(j+3), 0.75) for j in filter_list])
+    probs /= probs.sum()
 
-    # zipf distribution for queries
-    random.seed(10)
-    np.random.seed(10)
-    sum = 10000
+    # Probabilities for number of filters per query
+    max_filters = 10
+    count_list = list(range(3, max_filters))
+    count_probs = np.array([1/(j+3) for j in count_list])
+    count_probs /= count_probs.sum()
+
+    # Generate query templates using Zipf distribution
+    random.seed(10)  # Alternate dataset uses seed=42
+    np.random.seed(10)  # Alternate dataset uses seed=42
+    total_queries = 10000
     q_list = []
     templates = {}
     zipf_coef = 2500
-    while len(q_list) < sum:
-        i = 0
-        # Choose a query template
-        num_filters = np.random.choice(count_list, size=1, replace=False,p=count_probs)
-        template = np.random.choice(filter_list, size=num_filters[0], replace=False, p=probs)
-        if frozenset(template) in templates:
+
+    while len(q_list) < total_queries:
+        num_filters = random.randint(min_filters, max_filters)
+        template = tuple(sorted(np.random.choice(filter_list, num_filters, replace=False, p=probs)))
+        if template in templates:
             continue
-        # template = random.sample(filter_list,20,weights=[1/(j+3) for j in range(200000)])
-        while i < sum / zipf_coef / 9:
-            i += 1
+
+        ount = 0
+        max_count = total_queries / curr_zipf_coef / 9
+        while count < max_count and len(q_list) < total_queries:
+            count += 1
             q_list.append(template)
-            if len(q_list) >= sum:
-                break
-        zipf_coef -= 1
-        templates[frozenset(template)] = i
-    print(zipf_coef)
+        curr_zipf_coef = max(1, curr_zipf_coef - 1)
+        templates[template] = count
 
-    spmat = csc_matrix(spmat)
-    sel = 0
-    for k, v in templates.items():
-        print(k)
-        idx_set = set()
-        for f in k:
-            idx_set = idx_set.union(set(spmat.indices[spmat.indptr[f]:spmat.indptr[f+1]]))
-        sel += len(idx_set) * v
-        print(len(idx_set))
-    print("selectivity:", sel)
+    print("unique templates:", len(templates))
 
+    sels = []
+    total_selectivity = 0
+    for template, count in templates.items():
+        setlist = [set(data_filters_csc.indices[data_filters_csc.indptr[f]:data_filters_csc.indptr[f+1]]) for f in template]
+        selectivity = len(idx_set)
+        total_selectivity += len(idx_set) * count
+        sels.extend([selectivity] * count)
+
+    print("Avg. selectivity:", total_selectivity / total_queries)
+
+    sels.sort()
+    print("min:", sels[0])
+    print("p20:", sels[2000])
+    print("p40:", sels[4000])
+    print("p60:", sels[6000])
+    print("p80:", sels[8000])
+    print("max:", sels[9999])
+
+    # Convert query list to sparse matrix
     random.shuffle(q_list)
-
-    query_filters_lil_matrix = lil_matrix((10000, 200000), dtype=np.int32)
-    for i in range(10000):
-        for j in q_list[i]:
-            query_filters_lil_matrix[i,j] = 1
-    query_filters_csc_matrix = csc_matrix(query_filters_lil_matrix)
+    query_filters = lil_matrix((total_queries, n_filters), dtype=np.int32)
+    for i, template in enumerate(q_list):
+        query_filters.rows[i] = list(template)
+        query_filters.data[i] = [1] * len(template)
     
-    pickle.dump(query_filters_csc_matrix, open("~/bigann/biganntest/data/uqv/uqv_query_attrs.pkl", "wb"))
+    pickle.dump(csc_matrix(query_filters), open("biganntest/data/uqv/uqv_query_attrs.pkl", "wb"))
